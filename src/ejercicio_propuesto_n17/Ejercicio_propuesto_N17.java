796d02e9f42bcafcc3932aa2d310bddfade92304
Christopher <Christopher@DESKTOP-6E1TUBO>
2024-02-28 21:12:58 -0500

    Ejercicio Propuesto No 17

---
+++ src/ejercicio_propuesto_n17/Ejercicio_propuesto_N17.java
@@ -0,0 +1,12 @@
+package ejercicio_propuesto_n17;
+public class Ejercicio_propuesto_N17 {
+    public static void main(String[] args) {
+        double Radio , Area , Longitud;
+        Radio = 15;
+        Area = Math.PI * Math.pow(Radio,2);
+        Longitud = 2 * Math.PI * Radio;
+        System.out.println("El radio del círculo es : " + Radio);
+        System.out.println("El área del círculo es : " + Area);
+        System.out.println("La longitud de la circunferencia es : " + Longitud);
+    }
+}
