36e71e40d3e2dfd7dd806db229a8729da6da9ef2
Christopher <Christopher@DESKTOP-6E1TUBO>
2024-02-28 19:16:10 -0500

    Updated

---
+++ src/ejercicio_resuelto_n5/Ejercicio_resuelto_N5.java
@@ -0,0 +1,14 @@
+
+package ejercicio_resuelto_n5;
+public class Ejercicio_resuelto_N5 {
+    public static void main(String[] args) {
+    double SUMA,X,Y;
+    SUMA = 0;
+    X =20;
+    SUMA = SUMA + X;
+    Y = 40;
+    X = X + Math.pow(Y,2);
+    SUMA = SUMA + (X/Y);
+    System.out.println("El valor de la suma es : " + SUMA);
+    }
+}
